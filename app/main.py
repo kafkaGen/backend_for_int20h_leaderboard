c268e6d24b70986319a927c7e3d3f5a6850a0e38
Oleh Borysevych <borysevych.oleh87@gmail.com>
2024-02-07 10:58:28 +0200

    add https protocol

--- app/main.py
+++ app/main.py
@@ -1,9 +1,15 @@
+import ssl
+
+import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from .routers import leaderboard, login
 
 app = FastAPI()
+
+ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+ssl_context.load_cert_chain("cert.pem", keyfile="key.pem")
 
 app.add_middleware(
     CORSMiddleware,
@@ -15,3 +21,6 @@ app.add_middleware(
 
 app.include_router(login.router)
 app.include_router(leaderboard.router)
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=5000, ssl=ssl_context)
